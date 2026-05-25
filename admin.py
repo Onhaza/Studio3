253d899580dd4342ca1fd98bca061640ff378850
Kusuma Y <Kusuma.y@kkumail.com>
2026-05-25 22:09:01 +0700

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from myapp.models import Student, Staff, Admin, Equipment, Borrowing
 
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Staff)
-admin.site.register(Admin)
-admin.site.register(Equipment)
-admin.site.register(Borrowing)
